89885e0fc76b53afd56d40f694609b9e39294ee4
Joshua Joseph <tuf09815@temple.edu>
2023-02-07 12:16:57 -0500

    Activity 2

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,15 +14,30 @@ class MainActivity : AppCompatActivity() {
         val spinner = findViewById<Spinner>(R.id.spinner)
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
-        /* Step 1: Populate this array */
-        //val numberArray = Array Of Numbers
+        /* Step 1: Populate this array w/number 1-100 */
+        val numberArray = IntArray(100)
+        for(i in 1..100)
+        {
+            numberArray[i-1] = i
+            //println(numberArray[i])
+        }
 
         /* Step 2: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray.asList())
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                p0?.run{
+                    displayTextView.textSize = getItemAtPosition(p2).toString().toFloat()
+                }
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+        }
 
     }
 }
